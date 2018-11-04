dd6cc4c782629ed00e01fa58a27d2157be18e585
Sampath Sitinamaluwa <sampathsris@gmail.com>
2018-11-04 23:00:07 +0530

    Added a counter example.

---
+++ Examples/Counter/CounterExampleCore/Counter.cs
@@ -0,0 +1,54 @@
+﻿
+namespace CounterExampleCore
+{
+    public static class Counter
+    {
+        const string INCREMENT = "INCREMENT";
+        const string DECREMENT = "DECREMENT";
+        const string CHANGE_BY = "CHANGE_BY";
+
+        static readonly Redux.Action ACTION_INCREMENT = new Redux.Action(INCREMENT);
+        static readonly Redux.Action ACTION_DECREMENT = new Redux.Action(DECREMENT);
+
+        public static Redux.Action Increment()
+        {
+            return ACTION_INCREMENT;
+        }
+
+        public static Redux.Action Decrement()
+        {
+            return ACTION_DECREMENT;
+        }
+
+        public static Redux.Action ChangeBy(int amount)
+        {
+            if (amount == 1)
+            {
+                return Increment();
+            }
+            else if (amount == -1)
+            {
+                return Decrement();
+            }
+            else
+            {
+                return new Redux.Action<int>(CHANGE_BY, amount);
+            }
+        }
+
+        public static int Reduce(int state, Redux.Action action)
+        {
+            switch (action.Type)
+            {
+                case INCREMENT:
+                    return state + 1;
+                case DECREMENT:
+                    return state - 1;
+                case CHANGE_BY:
+                    return state + (action as Redux.Action<int>).Payload;
+                default:
+                    return state;
+            }
+        }
+    }
+}
---
+++ Examples/Counter/CounterExampleVanilla/Program.cs
@@ -0,0 +1,38 @@
+﻿using CounterExampleCore;
+using Redux;
+using System;
+
+namespace CounterExample
+{
+    class Program
+    {
+        private static IStore counterStore = null;
+
+        static void Main(string[] args)
+        {
+            counterStore = Redux.Redux.CreateStore<int>(Counter.Reduce);
+
+            Console.WriteLine("Initial state: " + Redux.Redux.GetState<int>(counterStore)); // 0
+
+            SendAction(Counter.Increment()); // 1
+            SendAction(Counter.Decrement()); // 0
+            SendAction(Counter.Increment()); // 1
+            SendAction(Counter.Increment()); // 2
+
+            // Shouldn't output anything.
+            SendAction(new Redux.Action("DUMMY_ACTION"));
+
+            SendAction(Counter.ChangeBy(-3)); // -1
+            SendAction(Counter.ChangeBy(5));  // 4
+
+            Console.ReadKey();
+        }
+
+        private static void SendAction(Redux.Action action)
+        {
+            Console.WriteLine("Dispatching action: " + action);
+            counterStore.Dispatch(action);
+            Console.WriteLine("State: " + Redux.Redux.GetState<int>(counterStore));
+        }
+    }
+}
