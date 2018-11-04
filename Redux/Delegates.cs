5ce66991eb7e7fa35ff559bfd85aad3d4273b82f
Sampath Sitinamaluwa <sampathsris@gmail.com>
2018-11-04 21:02:01 +0530

    Added utility class Redux and a delegate representing a reducer.

---
+++ Redux/Delegates.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Redux
+{
+    /// <summary>
+    /// A function that is a Redux reducer.
+    /// </summary>
+    /// <typeparam name="T">Type of the reduced state.</typeparam>
+    /// <param name="state">State before the reduction.</param>
+    /// <param name="action">Action used for the reduction.</param>
+    /// <returns>State after the reduction.</returns>
+    public delegate T Reducer<T>(T state, Action action);
+}
---
+++ Redux/Redux.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Redux
+{
+    public static class Redux
+    {
+        /// <summary>
+        /// Creates a Redux store that serves a given type T.
+        /// </summary>
+        /// <typeparam name="T">Type of the reduced state.</typeparam>
+        /// <param name="reducer">Reducer function.</param>
+        /// <returns>The created Redux store.</returns>
+        public static IStore CreateStore<T>(Reducer<T> reducer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
