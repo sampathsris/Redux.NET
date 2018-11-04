7cefdcb3ac07291bd8c09db2010e4daefbb88672
Sampath Sitinamaluwa <sampathsris@gmail.com>
2018-11-04 20:48:07 +0530

    Created project Redux with definitions for Action and IStore.

---
+++ Redux/Action.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Redux
+{
+    /// <summary>
+    /// Represents a Redux action with no accompanying payload.
+    /// </summary>
+    public class Action
+    {
+        /// <summary>
+        /// Every Redux action has a mandatory Type property.
+        /// </summary>
+        public string Type
+        {
+            get;
+            // Only the constructor may set the action's Type during initialization.
+            private set;
+        }
+
+        /// <summary>
+        /// Creates an Action object.
+        /// </summary>
+        /// <param name="type">Type of the Redux action.</param>
+        public Action(string type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException("Type", "Type must not be null when creating an Action.");
+            }
+
+            Type = type;
+        }
+
+        public override string ToString()
+        {
+            return "Redux.Action, Type: " + Type;
+        }
+    }
+
+    /// <summary>
+    /// A generic action that can carry a payload of a given type.
+    /// </summary>
+    /// <typeparam name="T">Type of payload</typeparam>
+    public class Action<T> : Action
+    {
+        public T Payload { get; private set; }
+
+        public Action(string type, T payload)
+            : base(type)
+        {
+            Payload = payload;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ", Payload: " + Payload;
+        }
+    }
+}
---
+++ Redux/IStore.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Redux
+{
+    public interface IStore
+    {
+        /// <summary>
+        /// Dispatches an action to the Store.
+        /// </summary>
+        /// <param name="action">Action to be dispatched.</param>
+        void Dispatch(Action action);
+    }
+}
