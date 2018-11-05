d3c7b40dbc9b73a923934a518750b35d122995c5
Sampath Sitinamaluwa <sampathsris@gmail.com>
2018-11-05 08:32:08 +0530

    Added combined example. Much to implement.

---
+++ Examples/CombinedApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using CounterExampleCore;
+using ToDoListExampleCore;
+using Redux;
+
+namespace CombinedApp
+{
+    class Program
+    {
+        private static IStore combinedStore = null;
+
+        static void Main(string[] args)
+        {
+            IStore counter1Store = Redux.Ops.CreateStore<int>(Counter.Reduce);
+            IStore counter2Store = Redux.Ops.CreateStore<int>(Counter.Reduce);
+            IStore todoListStore = Redux.Ops.CreateStore<IDictionary<string, bool>>(ToDoList.Reduce);
+
+            IStore combinedStore = null; //TODO: Create combined store.
+            // TODO: Subscribe to store.
+            //combinedCounter.StateChanged += combinedStore_StateChanged;
+
+            //Console.WriteLine("Initial state: " + combinedCounter.State); // 0
+
+            SendAction(Counter.Increment()); // 1
+            SendAction(Counter.Decrement()); // 0
+            SendAction(Counter.Increment()); // 1
+            SendAction(Counter.Increment()); // 2
+
+            // Shouldn't output anything.
+            SendAction(new Redux.Action("DUMMY_ACTION"));
+
+            SendAction(Counter.ChangeBy(-3)); // -1
+            SendAction(Counter.ChangeBy(5));  // 4
+
+            string a = "Learn Redux";
+            string b = "Play with Redux";
+            string c = "Implement Redux in .NET";
+
+            SendAction(ToDoList.AddTodo(a));
+            SendAction(ToDoList.AddTodo(b));
+            SendAction(ToDoList.AddTodo(c));
+
+            SendAction(ToDoList.ToggleTodo(a));
+            SendAction(ToDoList.ToggleTodo(b));
+            SendAction(ToDoList.ToggleTodo(b));
+            Console.ReadKey();
+        }
+
+        private static void SendAction(Redux.Action action)
+        {
+            Console.WriteLine("Dispatching action: " + action);
+            combinedStore.Dispatch(action);
+        }
+    }
+}
---
+++ Examples/CombinedApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CombinedApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CombinedApp")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("aaabfc0f-b638-4ec8-af3a-758f99a6614c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
