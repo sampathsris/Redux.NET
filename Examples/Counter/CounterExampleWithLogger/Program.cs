b5cf1556c404f5d8b949776802c690829c60c345
Sampath Sitinamaluwa <sampathsris@gmail.com>
2018-11-10 19:00:20 +0530

    Minor refactoring.

--- Examples/Counter/CounterExampleWithLogger/Program.cs
+++ Examples/Counter/CounterExampleWithLogger/Program.cs
@@ -1,10 +1,6 @@
 ﻿using CounterExampleCore;
 using Redux;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CounterExampleWithLogger
 {
--- Redux/GlobalSuppressions.cs
+++ Redux/GlobalSuppressions.cs
@@ -9,16 +9,10 @@
 // You do not need to add suppressions to this file manually.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Redux", Scope = "type", Target = "Redux.ReduxAction`1")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "api", Scope = "type", Target = "Redux.Middleware`1")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Api", Scope = "type", Target = "Redux.MiddlewareApi`1")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Stdout", Scope = "member", Target = "Redux.StandardMiddleware.#CreateStdoutLoggerMiddleware`1()")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Redux")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Redux", Scope = "namespace", Target = "Redux")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Redux", Scope = "type", Target = "Redux.ReduxAction")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA2210:AssembliesShouldHaveValidStrongNames")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Api", Scope = "type", Target = "Redux.IReduxDispatcherApi`1")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Redux", Scope = "type", Target = "Redux.IReduxDispatcherApi`1")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "api", Scope = "type", Target = "Redux.ReduxThunk`1")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Redux", Scope = "type", Target = "Redux.ReduxThunk`1")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Redux", Scope = "type", Target = "Redux.ReduxThunk`2")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "api", Scope = "type", Target = "Redux.ReduxThunk`2")]
