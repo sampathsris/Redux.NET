df5b02d21e4e4b6e0e05c1b723fece11cad1803a
Sampath Sitinamaluwa <sampathsris@gmail.com>
2018-11-12 06:32:39 +0530

    Added a winforms example.

---
+++ Examples/Counter/CounterWinformsVanilla/CounterForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace CounterWinformsVanilla
+{
+    partial class CounterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDecrement = new System.Windows.Forms.Button();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.btnIncrement = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDecrement
+            // 
+            this.btnDecrement.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDecrement.Location = new System.Drawing.Point(12, 12);
+            this.btnDecrement.Name = "btnDecrement";
+            this.btnDecrement.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrement.TabIndex = 0;
+            this.btnDecrement.Text = "-";
+            this.btnDecrement.UseVisualStyleBackColor = true;
+            this.btnDecrement.Click += new System.EventHandler(this.btnDecrement_Click);
+            // 
+            // lblCount
+            // 
+            this.lblCount.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblCount.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCount.Location = new System.Drawing.Point(93, 12);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(125, 23);
+            this.lblCount.TabIndex = 1;
+            this.lblCount.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnIncrement
+            // 
+            this.btnIncrement.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIncrement.Location = new System.Drawing.Point(224, 12);
+            this.btnIncrement.Name = "btnIncrement";
+            this.btnIncrement.Size = new System.Drawing.Size(75, 23);
+            this.btnIncrement.TabIndex = 2;
+            this.btnIncrement.Text = "+";
+            this.btnIncrement.UseVisualStyleBackColor = true;
+            this.btnIncrement.Click += new System.EventHandler(this.btnIncrement_Click);
+            // 
+            // CounterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 45);
+            this.Controls.Add(this.btnIncrement);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.btnDecrement);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CounterForm";
+            this.Text = "Counter";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDecrement;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.Button btnIncrement;
+    }
+}
+
---
+++ Examples/Counter/CounterWinformsVanilla/CounterForm.cs
@@ -0,0 +1,52 @@
+﻿using CounterExampleCore;
+using Redux;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CounterWinformsVanilla
+{
+    public partial class CounterForm : Form
+    {
+        public CounterForm()
+        {
+            InitializeComponent();
+            SetupReduxStore();
+        }
+
+        IStore store;
+
+        private void SetupReduxStore()
+        {
+            store = Ops.CreateStore<int>(new Counter());
+            store.Subscribe<int>(StoreStateChanged);
+            SetLabel(store.GetState<int>());
+        }
+
+        private void StoreStateChanged(IStore store, int state)
+        {
+            SetLabel(state);
+        }
+
+        private void SetLabel(int count)
+        {
+            lblCount.Text = count.ToString();
+        }
+
+        private void btnDecrement_Click(object sender, EventArgs e)
+        {
+            store.Dispatch(Counter.Decrement());
+        }
+
+        private void btnIncrement_Click(object sender, EventArgs e)
+        {
+            store.Dispatch(Counter.Increment());
+        }
+    }
+}
