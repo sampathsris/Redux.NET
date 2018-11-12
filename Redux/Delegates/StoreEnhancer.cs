a950fc942b8fbb0adc5ffe569401314550ddfec5
Sampath Sitinamaluwa <sampathsris@gmail.com>
2018-11-12 06:57:56 +0530

    Refactoring.

---
+++ Redux/Delegates/StoreEnhancer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Redux
+{
+    /// <summary>
+    /// Represents a function that accepts a StoreCreator, enhances it,
+    /// and returns a new store creator.
+    /// </summary>
+    /// <typeparam name="TState">Type of the stored state.</typeparam>
+    /// <param name="storeCreator">StoreCreator to be enhanced.</param>
+    /// <returns>Enhanced StoreCreator.</returns>
+    public delegate StoreCreator<TState> StoreEnhancer<TState>(
+        StoreCreator<TState> storeCreator
+    );
+}
